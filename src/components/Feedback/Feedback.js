6793a384275cdc471e3d882b5259509d87b3a98e
Vadym Usov <onlinemovement@gmail.com>
2021-01-17 18:59:27 +0200

    edits with the hook useState

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import grades from './grades.json';
 
@@ -7,63 +7,52 @@ import Statistics from './Statistics';
 import Buttons from './Buttons';
 import Notification from './Notification';
 
-class Feedback extends Component {
-  state = {
-    raiting: grades.reduce(
+function Feedback() {
+  const [raiting, setRaiting] = useState(() =>
+    grades.reduce(
       (options, grade) => ({
         ...options,
         [grade.text]: parseInt(grade.raiting),
       }),
       {},
     ),
+  );
+
+  const addRatingEvent = e => {
+    setRaiting({
+      ...raiting,
+      [e.target.innerText]: raiting[e.target.innerText] + 1,
+    });
   };
 
-  addRatingEvent = e => {
-    this.setState(({ raiting }) => ({
-      raiting: {
-        ...raiting,
-        [e.target.innerText]: raiting[e.target.innerText] + 1,
-      },
-    }));
+  const countTotalFeedback = () => {
+    return Object.values(raiting).reduce((acc, value) => acc + value);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state.raiting).reduce(
-      (acc, value) => acc + value,
-    );
-  };
-
-  countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = () => {
     let result = null;
-    result = Math.round(
-      this.state.raiting.good / (this.countTotalFeedback() / 100),
-    );
+    result = Math.round(raiting.good / (countTotalFeedback() / 100));
     return result ? result : 0;
   };
 
-  render() {
-    return (
-      <div>
-        <Section title={'Please leave feedback'}>
-          <Buttons
-            event={this.addRatingEvent}
-            options={Object.keys(this.state.raiting)}
+  return (
+    <div>
+      <Section title={'Please leave feedback'}>
+        <Buttons event={addRatingEvent} options={Object.keys(raiting)} />
+      </Section>
+      <Section title={'Statistics'}>
+        {countTotalFeedback() ? (
+          <Statistics
+            options={raiting}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback() ? (
-            <Statistics
-              options={this.state.raiting}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification msg="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification msg="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default Feedback;
